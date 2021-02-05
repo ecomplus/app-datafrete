e5d62bd0d01faa04012ca836190625a33cfb9cbe
Leonardo Matos <leomp120894@gmail.com>
2021-02-05 16:59:30 -0300

    perf(update-tokens): disable scheduled function (no auth needed)

--- functions/index.js
+++ functions/index.js
@@ -133,10 +133,12 @@ exports[functionName] = functions.https.onRequest(server)
 console.log(`-- Starting '${app.title}' E-Com Plus app with Function '${functionName}'`)
 
 // schedule update tokens job
-const cron = '15 5,13,21 * * *'
+// const cron = '15 */3 * * *'
+/*
 exports.updateTokens = functions.pubsub.schedule(cron).onRun(() => {
   return prepareAppSdk().then(appSdk => {
     return appSdk.updateTokens()
   })
 })
 console.log(`-- Sheduled update E-Com Plus tokens '${cron}'`)
+*/
